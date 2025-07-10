9ea9822929de3c1027aad04c997f5363a791b675
HARSHITA <harshitabaghel2005@gmail.com>
2025-07-10 15:25:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Trapping Rain Water/trapping-rain-water.cpp
+++ Difficulty: Hard/Trapping Rain Water/trapping-rain-water.cpp
@@ -1,28 +1,24 @@
 class Solution {
   public:
-    int maxWater(vector<int> &height) {
+    int maxWater(vector<int> &arr) {
         // code here
-        int n = height.size();
-  int left = 0, right = n - 1;
-  int res = 0;
-  int maxLeft = 0, maxRight = 0;
-  while (left <= right) {
-    if (height[left] <= height[right]) {
-      if (height[left] >= maxLeft) {
-        maxLeft = height[left];
-      } else {
-        res += maxLeft - height[left];
-      }
-      left++;
-    } else {
-      if (height[right] >= maxRight) {
-        maxRight = height[right];
-      } else {
-        res += maxRight - height[right];
-      }
-      right--;
-    }
-  }
-  return res;
+        int n = arr.size();
+        if(n<3) return 0;
+        vector<int> left(n);
+        vector<int> right(n);
+        left[0]=arr[0];
+        right[n-1] = arr[n-1];
+        for(int i=1;i<n;i++){
+            left[i]= max(left[i-1],arr[i]);
+        }
+        for(int i=n-2;i>=0;i--){
+            right[i]= max(right[i+1],arr[i]);
+        }
+        
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans= ans+ (min(left[i],right[i])-arr[i]) ;
+        }
+        return ans;
     }
 };
