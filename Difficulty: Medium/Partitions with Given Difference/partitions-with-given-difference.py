a3730a5ae23ca11a0220b711a88062f4a909db2c
HARSHITA <harshitabaghel2005@gmail.com>
2025-09-17 10:35:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.py
@@ -0,0 +1,41 @@
+
+from typing import List
+
+
+class Solution:
+    def findways(self,arr,target):
+        n= len(arr)
+        MOD = 10**9 + 7
+        dp = [[0] * (target + 1) for _ in range(n)]
+        
+        for i in range (n):
+            dp[i][0]=1
+        if arr[0]<= target:
+            dp[0][arr[0]]=1
+        if arr[0] == 0:
+            dp[0][0] = 2
+        for i in range(n):
+            for tar in range(target+1):
+                notake = dp[i-1][tar]
+                take = 0
+                if arr[i]<=tar:
+                    take = dp[i-1][tar-arr[i]]
+                dp[i][tar]= (take + notake)%MOD
+        return (dp[n-1][target])%MOD
+    def countPartitions(self, arr, d):
+        # code here
+        n= len(arr)
+        total=0
+        for i in range(n):
+            total+=arr[i]
+                
+        if ((total+d) ==0 or (total+d)%2 ):
+            return 0
+        target = (total + d) // 2
+        
+        # Target should be between 0 and total
+        if target < 0 or target > total:
+            return 0
+        return self.findways(arr,target)
+        
+        
